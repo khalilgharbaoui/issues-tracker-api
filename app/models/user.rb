3dd6384a80affe852bb63b5b0fa6506d18216ef9
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-05 01:49:37 +0200

    Improve user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Model associations
-  has_many :issues, foreign_key: :created_by
+  has_many :issues, :class_name => "Issue", :foreign_key => "created_by"
+  has_many :assigned_issues, :class_name => "Issue", :foreign_key => "assigned_to"
+
   # Validations
   validates_presence_of :name, :email, :password_digest
   validates :email, uniqueness: true
