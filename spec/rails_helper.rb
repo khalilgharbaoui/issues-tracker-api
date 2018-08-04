c1581fecf1f81ae2f12529b6e5adc41a1f7c0fc0
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-04 19:57:44 +0200

    Add controller spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,8 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
