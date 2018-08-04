ab140d0c010ab8be34f34161800d3805a3c0a028
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-05 01:58:52 +0200

    [WIP] Fix index, create methods and issue_params in issues_controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -3,14 +3,19 @@ class IssuesController < ApplicationController
 
   # GET /issues
   def index
-    @issues = Issue.all
-    render json: @issues, status: :ok
+    if current_user.manager?
+      @issues = Issue.all.order("created_at DESC")
+      render json: @issues, status: :ok
+    else
+      @issues = current_user.issues.order("created_at DESC")
+      render json: @issues, status: :ok
+    end
   end
 
   # POST /issues
   def create
     begin
-      @issue = Issue.create!(issue_params)
+      @issue = current_user.issues.create!(issue_params)
       render json: @issue, status: :created
     rescue Exception => e
       # raise e
@@ -39,9 +44,19 @@ class IssuesController < ApplicationController
 
   def set_issue
     @issue = Issue.find(params[:id])
+    # TODO:
+    # begin
+    #   @issue = current_user.issues.find(params[:id])
+    # rescue ActiveRecord::RecordNotFound => e
+    #   json_response({ message: "You can only Show, Update or Delete what is yours! no cheating!" }, :unauthorized)
+    # end
   end
 
   def issue_params
-    params.permit(:title, :created_by, :assigned_to, :status)
+    if current_user.manager?
+      params.require(:issue).permit(:assigned_to, :status)
+    else
+      params.require(:issue).permit(:title)
+    end
   end
 end
