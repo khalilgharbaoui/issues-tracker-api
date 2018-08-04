0219b0dd5965486defeb8d81c805fd8f4861ca21
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-05 01:53:18 +0200

    Add default of null to issue assigned_to attribute

--- db/migrate/20180802223429_create_issues.rb
+++ db/migrate/20180802223429_create_issues.rb
@@ -3,7 +3,7 @@ class CreateIssues < ActiveRecord::Migration[5.2]
     create_table :issues do |t|
       t.string :title
       t.string :created_by
-      t.string :assigned_to
+      t.string :assigned_to, null: true
       t.string :status, default: "pending"
 
       t.timestamps
