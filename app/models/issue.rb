65f17a05a165d709fbd66d888e9a924047a9d456
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-05 01:50:37 +0200

    Improve Issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,6 @@
 class Issue < ApplicationRecord
 
-  validates_presence_of :title, :created_by, :assigned_to, :status
+  validates_presence_of :title, :created_by, :status
+  belongs_to :user, :class_name => "User", :foreign_key => 'created_by'
+  belongs_to :manager, :class_name => "User", :foreign_key => 'assigned_to', optional: true
 end
