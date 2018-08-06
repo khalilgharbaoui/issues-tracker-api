9b361ac2596791fcad3d87f109b448ee0e17d5fc
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-06 06:24:06 +0200

    Comment out model association for now

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,3 +1,6 @@
 class IssueSerializer < ActiveModel::Serializer
   attributes :id, :title, :created_by, :assigned_to, :status, :created_at, :updated_at
+
+  # model association
+    # belongs_to :user
 end
