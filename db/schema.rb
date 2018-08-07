39a73f1aa7cdd9e01cbc6fb3dcc7fbdcc7129fca
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-07 22:19:10 +0200

    put back sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_08_03_030847) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "issues", force: :cascade do |t|
     t.string "title"
     t.string "created_by"
