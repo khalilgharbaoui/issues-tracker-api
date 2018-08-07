a6379d956a76335758f330442fb0f99e042f4092
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-07 12:12:17 +0200

    Extend token validity with 20 years!

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   # secret to encode and decode token
   HMAC_SECRET = Rails.application.credentials.dig(:secret_key_base)
 
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, exp = 20.years.from_now)
     # set expiry to 24 hours from creation time
     payload[:exp] = exp.to_i
     # sign token with application secret
