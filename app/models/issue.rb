4a59ab54ea7e5a18fd30c16ece3e81d65464ccd4
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-21 04:25:12 +0200

    Refactor Issue Model
    Logic moved from controller to model.
    
    Readable maintainable and clear!
    
    current_user! with a bing grabs the current user from the Current singleton object.
    
    on update stuff gets checked.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,80 @@
+# frozen_string_literal: true
+
+# This class ensures status and assignee business logic and has access to a per
+# request tread safe current user object, this is only used early in the cycle
+# before validation is done on update only.
 class Issue < ApplicationRecord
-  scope :status, -> (status) { where status: status }
-  validates_presence_of :title, :created_by, :status
-  belongs_to :user, :class_name => "User", :foreign_key => 'created_by'
-  belongs_to :manager, :class_name => "User", :foreign_key => 'assigned_to', optional: true
+  STATUSES = ['pending', 'in progress', 'resolved'].freeze
+
+  validates :title, :user_id, :status, presence: true
+  validates :status,
+            inclusion: {
+              in: STATUSES,
+              message: "%{value} is not in [#{STATUSES.join(', ')}]"
+            }, strict: ArgumentError
+
+  belongs_to :user, inverse_of: :issues
+  belongs_to :manager,
+             class_name: 'User', foreign_key: 'assigned_to', optional: true
+  before_validation :assignee_check, on: :update
+  before_validation :status_check, on: :update
+  scope :status, ->(status) { where status: status }
+
+  private
+
+  def assignee_check
+    same_assignee? || current_assignee? || blank_assignee? ? return : arg_err
+  end
+
+  def status_check
+    pending? || in_progress? || resolved? ? return : arg_err
+  end
+
+  def same_assignee?
+    assignee == existing_assignee
+  end
+
+  def current_assignee?
+    assignee_current_user? && existing_assignee.blank?
+  end
+
+  def blank_assignee?
+    assignee.blank? && existing_assignee.blank? || assignee.blank?
+  end
+
+  def assignee_current_user?
+    assignee.to_i == current_user!.id
+  end
+
+  def pending?
+    status == 'pending' && blank_assignee? || assignee_current_user?
+  end
+
+  def in_progress?
+    status == 'in progress' && !blank_assignee?
+  end
+
+  def resolved?
+    status == 'resolved' && !blank_assignee?
+  end
+
+  def assignee
+    assigned_to
+  end
+
+  def existing_assignee
+    issue.assigned_to
+  end
+
+  def issue
+    Issue.find(id)
+  end
+
+  def current_user!
+    Current.user
+  end
+
+  def arg_err
+    raise ArgumentError, "#{caller_locations(1..1).first.label} failed!"
+  end
 end
