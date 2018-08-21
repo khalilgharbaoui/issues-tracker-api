6b7b545291856f301d47a46897ebf9257f51eac7
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-21 03:12:01 +0200

    set option to raise unpermitted parameter error in config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # this raises ActionController::UnpermittedParameters when unpermited parameters are inserted!
+  config.action_controller.action_on_unpermitted_parameters = :raise
 end
