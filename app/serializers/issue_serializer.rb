fafb9b31c86d9f1368cd73d1ee45b63b76581b6c
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-21 03:10:44 +0200

    Replace created_by with user_id

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,5 +1,5 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :id, :title, :created_by, :assigned_to, :status, :created_at, :updated_at
+  attributes :id, :title, :user_id, :assigned_to, :status, :created_at, :updated_at
 
   # model association
     # belongs_to :user
